ffed707c8cf796eb3c8f1a69392d3786373cf89d
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-10-04 15:57:13 -0700

    Expanded prototype init and limited the function to a single global, one array of simulations

--- public/js/worker.js
+++ public/js/worker.js
@@ -1,66 +1,58 @@
 self.addEventListener('message', function(e) {
   var data = e.data;
-  switch (data.cmd) {
-    case 'start':
-      self.postMessage('WORKER STARTED: ' + data.msg);
-      break;
-    case 'stop':
-      self.postMessage('WORKER STOPPED: ' + data.msg + '. (buttons will no longer work)');
-      self.close(); // Terminates the worker.
-      break;
-    case 'grid':
-      var grid = data.grid
-      var iter = 0;
-      var coords = [];
-      var edge = data.edge;
-      var max_dist = 0;
-      for(var i=0; i < data.num; i++) {
-        var c = random_from_polar(edge, data.dim);
-        var x = c.x; var y = c.y;
-        while(coord_has_neighbor(grid, x, y)==0) {
-          var p = Math.floor(Math.random()*4)
-          switch(p) {
-            case 0: x = x+1; break;
-            case 1: x = x-1; break;
-            case 2: y = y+1; break;
-            case 3: y = y-1; break;
-          }
-          if(!valid_coords(grid,x,y) || too_far_away(x,y,edge, data.dim)) {
-            c = random_from_polar(edge, data.dim);
-            x = c.x;
-            y = c.y;
-          }
-          iter+=1;
+  if(data.cmd == 'grid') { //switch (data.cmd) {
+    var grid = data.grid
+    var iter = 0;
+    var coords = [];
+    var edge = data.edge;
+    var max_dist = 0;
+    for(var i=0; i < data.num; i++) {
+      var c = random_from_polar(edge, data.radius);
+      var x = c.x; var y = c.y;
+      while(coord_has_neighbor(grid, x, y, data.sticking_prob)==0) {
+        var p = Math.floor(Math.random()*4)
+        switch(p) {
+          case 0: x = x+1; break;
+          case 1: x = x-1; break;
+          case 2: y = y+1; break;
+          case 3: y = y-1; break;
         }
-        var dist = (x-data.dim/2)*(x-data.dim/2)+(y-data.dim/2)*(y-data.dim/2);
-        if(dist > max_dist) {max_dist = dist; edge = Math.sqrt(dist)+25; }
-        grid[x][y] += 1;
-        coords.push({'x':x, 'y':y});
+        if(!valid_coords(grid,x,y) || too_far_away(x,y,edge, data.radius, true)) {
+          c = random_from_polar(edge, data.radius);
+          x = c.x;
+          y = c.y;
+        }
+        iter+=1;
       }
-      self.postMessage({'iter':iter, 'coords':coords, 'edge':edge, 'max_dist':max_dist}); break;
-    default:
-      self.postMessage('Unknown command: ' + data.msg);
+      var dist = (x-data.radius)*(x-data.radius)+(y-data.radius)*(y-data.radius);
+      if(dist > max_dist) {max_dist = dist; edge = Math.sqrt(dist)+25; }
+      grid[x][y] += 1;
+      coords.push({'x':x, 'y':y});
+    }
+    self.postMessage({'iter':iter, 'coords':coords, 'edge':edge, 'max_dist':max_dist});
   };
 }, false);
 
-function too_far_away(x, y, radius, dim) {
-  x = x-dim/2;
-  y = y-dim/2;
-  r_squared = x*x+y*y;
-  theta = Math.atan(x,y);
-  return r_squared > radius*radius + 10000; //false; //(r-radius)*(r-radius) > 2500;
+function too_far_away(x, y, radius, radius, delete_far) {
+  if(delete_far) {
+    x = x-radius;
+    y = y-radius;
+    r_squared = x*x+y*y;
+    return r_squared > radius*radius + 10000; 
+  } else {
+    return false;
+  }
 }
-function random_from_polar(radius, dim) {
+function random_from_polar(r, radius) {
   var angle = Math.random()*2*Math.PI;
-  //radius = Math.min(radius, dim/2);
-  var x = Math.floor(radius*Math.cos(angle))+dim/2
-  var y = Math.floor(radius*Math.sin(angle))+dim/2
+  var x = Math.floor(r*Math.cos(angle))+radius
+  var y = Math.floor(r*Math.sin(angle))+radius
   return {'x':x, 'y':y};
 }
 function valid_coords(grid, x, y) {
   return grid[x] && grid[x][y] !== undefined;
 }
-function coord_has_neighbor(grid, x, y) {
+function coord_has_neighbor(grid, x, y, sticking_prob) {
   var num=0;
   num+= occupied(x,y)   ? 1: 0;
   num+= occupied(x-1,y) ? 1: 0;
@@ -71,6 +63,12 @@ function coord_has_neighbor(grid, x, y) {
   function occupied(x,y) {
     return grid[x] && grid[x][y];
   }
-  return num;
+  //TODO This a hack, basically saying the more neighbors you have the more likely
+  //you are to stay stuck.
+  if(num && Math.random() < sticking_prob*num) {
+    return num;
+  } else {
+    return 0;
+  }
 }
 
