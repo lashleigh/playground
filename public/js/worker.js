edf1e3a872dc2caac58b3696d68b41950253e700
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-10-06 21:49:53 -0700

    No longer using setInterval which means it is no longer possible to pause a simulation. It has gotten rid of the race condition that was causing particle duplication.

--- public/js/worker.js
+++ public/js/worker.js
@@ -1,10 +1,11 @@
 self.addEventListener('message', function(e) {
   var data = e.data;
-  if(data.cmd == 'grid') { //switch (data.cmd) {
+  if(data.cmd === 'grid') { 
     var grid = data.grid
-    var iter = 0;
-    var coords = [];
     var edge = data.edge;
+    var iter = 0;
+    var count = 0;
+    var coords = [];
     var max_dist = 0;
     var walk_function, neigh_func;
     if(data.grid_style==='hex') { 
@@ -17,11 +18,11 @@ self.addEventListener('message', function(e) {
       walk_function = classic_walk;
       neigh_func = has_neighbor;
     }
-    for(var i=0; i < data.num; i++) {
+    while(edge < data.radius) {
       var c = random_classic(edge, data.radius);
       while(neighbors(grid, c, data, neigh_func)==0) {
-        c = walk_function(c);
-        if(!valid_coords(grid,c,data.radius) || too_far_away(c,edge, data.radius, true)) {
+        c = walk_function(grid, c, data.radius);
+        if(!c || too_far_away(c,edge, data.radius, true)) {
           c = random_classic(edge, data.radius);
         }
         iter+=1;
@@ -30,9 +31,14 @@ self.addEventListener('message', function(e) {
       if(dist > max_dist) {max_dist = dist; edge = Math.sqrt(dist)+25; }
       grid[c.x][c.y]+= 1;
       coords.push(c);
+      count++;  
+      if(count % 20 === 0) {
+        self.postMessage({'status':'working', 'iter':iter, 'coords':coords, 'edge':edge, 'max_dist':max_dist});
+        coords = [];
+      }
     }
-    self.postMessage({'iter':iter, 'coords':coords, 'edge':edge, 'max_dist':max_dist});
-  };
+    self.postMessage({'status':'complete', 'grid':grid});
+  }
 }, false);
 
 function too_far_away(c, edge, radius, delete_far) {
@@ -46,65 +52,46 @@ function too_far_away(c, edge, radius, delete_far) {
   }
 }
 function random_classic(r, radius) {
+  // If r ever gets bigger than radius we have to worry about
+  // coordinates being generated out of bounds.
   var angle = Math.random()*2*Math.PI;
   var x = Math.floor(r*Math.cos(angle))+radius
   var y = Math.floor(r*Math.sin(angle))+radius
   return {'x':x, 'y':y};
 }
-function valid_coords(grid, c, r) {
-  return (c.x > 0) && (c.x < 2*r) && (c.y > 0) && (c.y<2*r) //_.isArray(grid[c.x]) && _.isNumber(grid[c.x][c.y]);
-}
-function valid_and_occupied(grid,x,y,r) {
-  return valid_coords(grid, {'x':x, 'y':y}, r) && (grid[x][y] > 0)
-}
 function occupied(grid, x, y) {
-  return grid[x][y] > 0
+  return grid[x] && grid[x][y] > 0 ? 1 : 0;
 }
-function has_neighbor(grid, c, data) {
+function has_neighbor(grid, c) {
   var num=0;
   var x = c.x, y=c.y;
-  var r = data.radius;
-  if( (x >=1) && (x <= 2*r-2) && (y >= 1) && (y <= 2*r-2)) {
-    num+= occupied(grid,x,y,r)   ? 1: 0;
-    num+= occupied(grid,x-1,y,r) ? 1: 0;
-    num+= occupied(grid,x+1,y,r) ? 1: 0;
-    num+= occupied(grid,x,y-1,r) ? 1: 0;
-    num+= occupied(grid,x,y+1,r) ? 1: 0;
-  } else {
-    num+= valid_and_occupied(grid,x,y,r)   ? 1: 0;
-    num+= valid_and_occupied(grid,x-1,y,r) ? 1: 0;
-    num+= valid_and_occupied(grid,x+1,y,r) ? 1: 0;
-    num+= valid_and_occupied(grid,x,y-1,r) ? 1: 0;
-    num+= valid_and_occupied(grid,x,y+1,r) ? 1: 0;
-  }
+  num+= occupied(grid,x,y);
+  num+= occupied(grid,x-1,y);
+  num+= occupied(grid,x+1,y);
+  num+= occupied(grid,x,y-1);
+  num+= occupied(grid,x,y+1);
   return num;
 }
-function has_neighbor_hex(grid, c, data) {
-  var num = has_neighbor(grid, c, data)
+function has_neighbor_hex(grid, c) {
+  var num = has_neighbor(grid, c)
   var x = c.x, y=c.y;
-  var r = data.radius;
   if(x%2==0) {
-    num+= valid_and_occupied(grid,x+1, y-1,r)
-    num+= valid_and_occupied(grid,x-1, y-1,r)
+    num+= occupied(grid,x+1, y-1)
+    num+= occupied(grid,x-1, y-1)
+    return num;
   } else {                   
-    num+= valid_and_occupied(grid,x-1, y+1,r)
-    num+= valid_and_occupied(grid,x+1, y+1,r)
+    num+= occupied(grid,x-1, y+1)
+    num+= occupied(grid,x+1, y+1)
+    return num;
   }
-  return num;
 }
-function has_neighbor_8(grid, c, data) {
-  var num = has_neighbor(grid, c, data)
-  // If there is no primary match then it can't stick
-  // Allowing particles to stick on only a diagonal
-  // match results in unrealistic cross hatch
-  //if(num>0) {
-    var x = c.x, y=c.y;
-    var r = data.radius;
-    num+= valid_and_occupied(grid,x+1,y-1,r) ? 1: 0;
-    num+= valid_and_occupied(grid,x+1,y+1,r) ? 1: 0;
-    num+= valid_and_occupied(grid,x-1,y-1,r) ? 1: 0;
-    num+= valid_and_occupied(grid,x-1,y+1,r) ? 1: 0;
-  //}
+function has_neighbor_8(grid, c) {
+  var num = has_neighbor(grid, c)
+  var x = c.x, y=c.y;
+  num+= occupied(grid,x+1,y-1);
+  num+= occupied(grid,x+1,y+1);
+  num+= occupied(grid,x-1,y-1);
+  num+= occupied(grid,x-1,y+1);
   return num;
 }                
 function neighbors(grid, c, data, neigh_func) {
@@ -119,7 +106,7 @@ function neighbors(grid, c, data, neigh_func) {
     return num;
   }
 }
-function classic_walk(c) {
+function classic_walk(grid, c, r) {
   var x=c.x, y=c.y;
   var p = Math.floor(Math.random()*4)
   switch(p) {
@@ -130,7 +117,7 @@ function classic_walk(c) {
   }
   return {'x':x, 'y':y};
 }
-function classic8_walk(c) {
+function classic8_walk(grid, c, r) {
   var x=c.x, y=c.y;
   var p = Math.floor(Math.random()*8)
   switch(p) {
@@ -145,7 +132,11 @@ function classic8_walk(c) {
   }
   return {'x':x, 'y':y};
 }
-
+var identity          = [[0,1], [1,0], [0, -1], [-1, 0]]
+var identity_for8     = [[0,1], [1,0], [0, -1], [-1, 0], [ 1, 1], [ 1,-1], [-1, -1], [-1, 1]]
+var identity_hex_even = [[0,1], [1,0], [0, -1], [-1, 0], [ 1,-1], [-1,-1]]
+var identity_hex_odd  = [[0,1], [1,0], [0, -1], [-1, 0], [-1, 1], [ 1, 1]]
+var identity_hex      = [identity_hex_even,identity_hex_odd]
 // Hexagonal stuff below here
 function random_hex(drop_r, max_r) {
   var z = Math.floor(Math.random()*drop_r*2)-drop_r
@@ -162,7 +153,7 @@ function random_hex(drop_r, max_r) {
   //var res = multiply(hex_identity[corner], drop_r)
   //return {'x':res[0], 'y':res[1], 'z':res[2]};
 }
-function hex_walk(c) {
+function hex_walk(grid, c, r) {
   var x=c.x, y=c.y;
   var p = Math.floor(Math.random()*6)
   switch(p) {
@@ -174,6 +165,27 @@ function hex_walk(c) {
     case 5: if(x%2==0) {x = x-1; y = y-1; } else {x = x+1; y = y+1;} break;
   }
   return {'x':x, 'y':y};
+}
+// Experimental looks for valid neighbors and then moves in a 
+// valid direction. Unfortunately it is very slow.
+function experimental_hex_walk(grid, c, r) {
+  var x = c.x, y = c.y;  
+  var valid_directions = []
+  for(var i=0; i < 6; i++) {
+    var id = identity_hex[x%2][i];
+    if(valid_and_empty(sum_array([x,y],id) )) {
+      valid_directions.push(id);
+    }
+  }
+  function valid_and_empty(v) {
+    return grid[v[0]] && grid[v[0]][v[1]] === 0; 
+  }
+  if(valid_directions.length) {
+    var p = Math.floor(Math.random()*valid_directions.length)
+    x += valid_directions[p][0]
+    y += valid_directions[p][1]
+    return {'x':x, 'y':y};
+  }  
 }
 // Ordered counter clockwise from
 // http://stackoverflow.com/questions/2049196/generating-triangular-hexagonal-coordinates-xyz
@@ -222,4 +234,22 @@ function random_corner(r, v) {
     return sum_array(c, multiply(hex_identity[id], p))
   }
 }
+function Particle(x, y) {
+  this.x = x;
+  this.y = y;
+  this.neighbors = [];
+  this.CalcNeighbors();
+  this.xMod2 = x % 2;
 
+  grid[x][y] = this;
+}
+Particle.prototype.CalcNeighbors = function() {
+  that = this;
+  this.neighbors = identity_hex.map(function(v) {return [v[0]+this.x,v[1]+this.y]})
+  this.neighbors.reject
+}
+Particle.prototype.UndateNeighbors = function() {
+  for(var i=0; i < this.neighbors.length; i++) {
+     
+  }
+}
